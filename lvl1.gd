66c6e6fb4ede5086f9934f8623a6e62de7351c1d
marchearth <bgra.coskn@gmail.com>
2018-07-20 02:57:43 +0300

    added screenshot

--- lvl1.gd
+++ lvl1.gd
@@ -1,15 +1,13 @@
 extends Node2D
 var playerheight = 10 #in pixels
 # /_|_\     | = 10    ,   _ =10/sqrt(3)   ,   / = 10*2/sqrt(3)
-var playerdot1 = Vector2( 180, 500) # player nose dot location
+var playerdot1 = Vector2(180, 500) # player nose dot location
 var playerdot2 = Vector2() # left corner 
-playerdot2.x = playerdot1.x - 
-
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	playerdot2.x = playerdot1.x - playerheight/sqrt(3)
+	playerdot2.y = playerdot1.y - playerheight
+	print(playerdot2)
