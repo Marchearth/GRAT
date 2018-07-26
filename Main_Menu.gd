f2ae8c41fa07d7a234bb45784da9f2304420a5a7
marchearth <bgra.coskn@gmail.com>
2018-07-27 02:48:48 +0300

    Added timed function to clear old waves, designed top and bottom menus with Görkem working on the menus.

--- Main_Menu.gd
+++ Main_Menu.gd
@@ -9,6 +9,8 @@ var max_limit = 1.0
 var min_limit = 0.1
 var screen_res = get_viewport()
 
+var test_array = ["hello", "mr", "array"]
+
 func _ready():
 	print(screen_res)
 	flicker_timer = Timer.new()
@@ -22,7 +24,11 @@ func _ready():
 	 'on_start_button_pressed')
 	get_node('credits_button').connect('pressed', self,
 	 'on_credits_button_pressed')
-	
+
+	test_array.remove(1)
+	print(test_array)
+	print(test_array[1])
+
 func on_timer_timeout():
 	if alpha_value <= min_limit:
 			fade_out = false
@@ -32,16 +38,16 @@ func on_timer_timeout():
 		alpha_value -= change_val
 	elif fade_out == false:
 		alpha_value += change_val
-		
+
 	#alpha_value = randf()*1+0 #random value for title alpha
 	get_node('title').set_opacity(alpha_value)
 	get_node('start_button').set_opacity(alpha_value)
-	get_node('credits_button').set_opacity(alpha_value)	
-	
+	get_node('credits_button').set_opacity(alpha_value)
+
 	get_node('title').update()
 	get_node('start_button').update()
 	get_node('credits_button').update()
-	
+
 func on_start_button_pressed():
 	print('changing to new scene')
 	alpha_value = null
--- lvl1.gd
+++ lvl1.gd
@@ -1,13 +1,17 @@
 extends Node2D
 # drawings move each frame some pixels thats speed
-var green = Color(0.633, 1.0, 0.0, 1.0)
+var usual_green = Color(0.633, 1.0, 0.0, 1.0)
+var energy_trace_green = Color(0.633, 1.0, 0.0, 0.5)
+
 var p_height = 25 #in pixels # /_|_\     | = 10    ,   _ =10/sqrt(3)   ,   / = 10*2/sqrt(3)
-var playerdot1 = Vector2(180,580) # player nose dot location
+var playerdot1 = Vector2(180,500) # player nose dot location
 var playerdot2 = Vector2() # left corner
 var playerdot3 = Vector2() # right corner
 var playerdot_arr = []
+
 onready var screen_size = get_node("background").get_rect().size
 onready var screen_width = screen_size.x
+
 var midline_arr = []
 var midline_alpha = 1.0
 var mov_left = false
@@ -23,6 +27,9 @@ var waves_arr = [] # the waves coming down
 var midline_x = 0
 var first_wave = Vector2()
 var start_point = 0
+
+var cleanup_timer
+var waitval = 0.2 # wait for 0.2 seconds
 
 func _ready():
 	set_fixed_process(true)
@@ -44,6 +51,13 @@ func _ready():
 	waves_arr[1].y = -absolute(waves_arr[1].x - screen_width/2)
 	print(waves_arr.size())
 
+	cleanup_timer = Timer.new()
+	cleanup_timer.set_wait_time(waitval)
+	cleanup_timer.set_one_shot(false)
+	cleanup_timer.connect("timeout", self, "ifed_destroy_old_wave")
+	# Call function when it timesout
+	add_child(cleanup_timer)
+	cleanup_timer.start()
 
 func ifed_create_new_wave():
 	if waves_arr[waves_arr.size()-1].y >= 0: # if last point is in the edge of the top screen
@@ -51,6 +65,11 @@ func ifed_create_new_wave():
 		waves_arr[waves_arr.size()-1].y = \
 		waves_arr[waves_arr.size()-2].y \
 		- absolute(waves_arr[waves_arr.size()-2].x - waves_arr[waves_arr.size()-1].x)
+
+func ifed_destroy_old_wave():
+	print("\n",waves_arr.size())
+	if waves_arr[0] != null && waves_arr[1].y > 640:
+		waves_arr.remove(0)
 
 func mov_waves(speed):
 	for i in range(0, waves_arr.size()):
@@ -81,10 +100,9 @@ func _draw():
 	midline_alpha), 0.01) #midline
 
 	draw_colored_polygon(player_coordinates(p_height),
-	Color(0.633, 1, 0, 1), Vector2Array([])) #player
-	for i in range(0, waves_arr.size()):
-		if i != waves_arr.size() - 1:
-			draw_line(waves_arr[i], waves_arr[i+1], green, 3)
+	usual_green, Vector2Array([])) #player
+	for i in range(0, waves_arr.size() - 1):
+		draw_line(waves_arr[i], waves_arr[i+1], energy_trace_green, 3)
 
 func on_button_left_pressed():
 	mov_left = true
